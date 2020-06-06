7d34aff82ae5c1bb43af0b1e3aae6902d1ef746e
eunbin <sag06051@naver.com>
2020-06-07 03:05:21 +0900

    History component 생성 및 export
    History component를 생성하고 export
    화면에 History 창 뜨는지 확인

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,6 +5,7 @@ import Panel from "./Panel";
 import Display from "./Display";
 import ButtonGroup from "./ButtonGroup";
 import Button from "./Button";
+import History from "./History";
 
 const Container = styled.div`
   margin: 30px auto;
@@ -164,7 +165,8 @@ class Calculator extends React.Component {
           </ButtonGroup>
         </Panel>
         {/* TODO: History componet를 이용해 map 함수와 Box styled div를 이용해 history 표시 */}
-
+	<History>
+	</History>
       </Container>
     );
   }
